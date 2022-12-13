41de9a9628b11199e828173b5af2cc9c9edcccf2
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-12 20:12:50 -0800

    add templates to separate controllers and views by models

---
+++ ProjectName/Controllers/ClassNameController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+// using ProjectName.Models;
+
+namespace ProjectName.Controllers
+{
+  public class ItemsController : Controllers
+  {
+    // Routes
+    // [HttpGet(")]
+    // [HttpPost("")]
+
+  }
+}
