ea05fca4cb87f0cf02802ee6c9aa20497d70f91f
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-12 20:03:25 -0800

    add boilerplate

---
+++ ProjectName/Controllers/NameController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+// using ProjectName.Models;
+
+namespace ProjectName.Controllers
+{
+  public class HomeController : Controllers
+  {
+    // Routes
+    // [HttpGet(")]
+    // [HttpPost("")]
+
+  }
+}
